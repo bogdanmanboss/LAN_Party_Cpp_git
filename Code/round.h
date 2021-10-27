8f92cb114a78345effe29b2ef840afa7a4f71ab7
bogdanmanboss <bogdanmanboss@DESKTOP-0Q9IBK4.localdomain>
2021-10-27 21:31:06 +0300

    Round Class Header

--- Code/round.h
+++ Code/round.h
@@ -1,4 +1,5 @@
 #include "team.h"
+//This represent the shutdown between 2 teams
 
 class Round
 {
@@ -6,13 +7,13 @@ class Round
     Team t1,t2;
     
     public:
-    Team get_win_team();
-    Team get_lose_team();
+    Team get_win_team(); //return the winnig team
+    Team get_lose_team(); //return the losing team
 
-    void set_round(const Team&, const Team&);
+    void set_round(const Team&, const Team&);//helps setting the round
 
-    friend ostream& operator <<(ostream &, Round&);
-    friend ofstream& operator <<(ofstream &, Round&);
+    friend ostream& operator <<(ostream &, Round&);//redefine this operator to make my life easier
+    friend ofstream& operator <<(ofstream &, Round&);//redefine this operator to make my life easier
 
 
 };
