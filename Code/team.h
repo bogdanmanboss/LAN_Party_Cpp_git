e51be1fddbebb4fd34656ab85b2f0f3184c98144
bogdanmanboss <bogdanmanboss@DESKTOP-0Q9IBK4.localdomain>
2021-10-27 21:31:29 +0300

    Team Class Header

--- Code/team.h
+++ Code/team.h
@@ -9,22 +9,25 @@ class Team
     Player *teammates;
 
     public:
-
+    //constructors
     Team();
     Team(int, string, Player*);
     Team(const Team&);
+    //destructor
     ~Team();
 
-    float get_team_score();
-    string get_team_name();
+    float get_team_score(); //calculates the score of the team : adds the score of all players than divide by the number of players
+    string get_team_name(); //return the team name
 
-    void modify_team_score();
+    void modify_team_score(); //modify the score of all players by increasing the score of all players by 1 
 
-    friend ifstream& operator >> (ifstream&, Team&);
-    friend ofstream& operator << (ofstream&, const Team&);
-    friend ostream& operator << (ostream&, const Team&);
+    friend ifstream& operator >> (ifstream&, Team&);//redefine this operator to make my life easier
+    friend ofstream& operator << (ofstream&, const Team&);  //redefine this operator to make my life easier
+                                                            //didn't implemet because it is almost the same as the next
 
-    Team& operator = (const Team&);
+    friend ostream& operator << (ostream&, const Team&);//redefine this operator to make my life easier
+
+    Team& operator = (const Team&);//redefine this operator to make my life easier
 
 
 };
